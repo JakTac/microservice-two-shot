0fcccb115a939cb1f0acfff6cd27db30fb3cba82
JakTac <jake.p.tippit@gmail.com>
2022-12-04 14:54:08 -0600

    Changes I guess?

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,13 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import LocationForm from './LocationForm';
 import ShoeList from './ShoeList';
-<<<<<<< HEAD
 import ShoeForm from './ShoeForm';
 import ShoeDetail from './ShoeDetail'
-=======
 import HatForm from './HatForm';
 import HatsList from './HatsList';
->>>>>>> main
 import Nav from './Nav';
 
 function App() {
@@ -20,13 +17,10 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/location/" element={<LocationForm />} />
           <Route path="/shoes/" element={<ShoeList />} />
-<<<<<<< HEAD
           <Route path="/shoes/:id" element={<ShoeDetail />} />
           <Route path='/shoes/new/' element={<ShoeForm />} />
-=======
           <Route path="/hat/" element={<HatForm />} />
           <Route path="/hats/" element={<HatsList />} />
->>>>>>> main
         </Routes>
       </div>
     </BrowserRouter>
