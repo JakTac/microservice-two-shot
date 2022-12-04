7e5142c84847a5d082f222a7ac5e73e269d9061f
Kramer Smith <kramersmith41@yahoo.com>
2022-12-04 00:01:47 -0600

    Delete shoe, alerts, and redirects complete

--- shoes/api/shoes_rest/urls.py
+++ shoes/api/shoes_rest/urls.py
@@ -6,5 +6,5 @@ from .views import (api_list_shoes, api_show_shoe)
 urlpatterns = [
     path('shoes/', api_list_shoes, name='api_create_shoes'),
     path('bins/<int:bin_vo_id>/shoes/', api_list_shoes, name='api_list_shoes'),
-    path('shoe/<int:pk>/', api_show_shoe, name='api_show_shoe'),
+    path('shoes/<int:pk>/', api_show_shoe, name='api_show_shoe'),
 ]
